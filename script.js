b8a0be180018a6e8bf24ad1a7bbe74d465fb2c6f
Ramz898 <ramziddin537@gmail.com>
2024-11-18 15:32:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let space = prompt ('Ismingizni kiriting')
+let spac =  prompt ('Emailingizni kiriting')
+let spa = prompt ('prolingizni kiriting')
+let user = {
+    name: 'Ramz',
+    emg: '5678',
+    parol:'3456'
+}
+console.log(user);
